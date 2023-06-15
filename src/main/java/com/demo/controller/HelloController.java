91d9708760e043c39da84425f4dad1fa341cbd1e
sakshi-dhyani <ibs.sakshi9@gmail.com>
2023-06-15 13:17:27 +0530

    testing

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -3,14 +3,21 @@ package com.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
 
 	
 	@GetMapping("/hello")
 	public String hello() {
+		System.out.println("hello");
 		return "hello this is demo";
 	}
+
+		
+
+
+	
 	
 	@GetMapping("/test")
 	public String testing() {
