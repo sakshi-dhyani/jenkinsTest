a4a41e18552c909aab17e09460dc13b16c0330cc
sakshi-dhyani <ibs.sakshi9@gmail.com>
2023-06-15 12:57:14 +0530

    add

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello this is demo";
+	}
+	
+	@GetMapping("/test")
+	public String testing() {
+		
+		return "testing";
+	}
+}
